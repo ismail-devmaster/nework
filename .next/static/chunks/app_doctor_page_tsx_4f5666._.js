a6886dda0b7e316a0ed26eb3dc395875463f8374
Ismail <ismailpcuse@gmail.com>
2024-11-04 23:38:33 +0100

    add doctor page

--- .next/static/chunks/app_doctor_page_tsx_4f5666._.js
+++ .next/static/chunks/app_doctor_page_tsx_4f5666._.js
@@ -5,11 +5,10 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/app_doctor_page_tsx_4f5666._.js",
   "chunks": [
-    "static/chunks/_4a410a._.js",
+    "static/chunks/_0d031c._.js",
     "static/chunks/node_modules_@radix-ui_react-icons_dist_react-icons_esm_ecd85e.js",
-    "static/chunks/node_modules_date-fns_91fece._.js",
-    "static/chunks/node_modules_react-day-picker_dist_index_esm_9fc304.js",
-    "static/chunks/node_modules_e8fb21._.js"
+    "static/chunks/node_modules_framer-motion_dist_es_34eb3c._.js",
+    "static/chunks/node_modules_9b0e64._.js"
   ],
   "source": "dynamic"
 });
