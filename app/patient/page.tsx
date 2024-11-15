21ba960a0c6d18739f36c0056e6127c9964f3216
xCyberpunkx <peeplil6666@gmail.com>
2024-11-15 20:44:06 +0100

    fixed pageProps type issues

--- app/patient/page.tsx
+++ app/patient/page.tsx
@@ -25,6 +25,9 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+interface PageProps {
+  isNewPatient?: boolean
+}
 const page = ({ isNewPatient = false }) => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   return (
